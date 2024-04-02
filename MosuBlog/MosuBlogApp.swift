f61838dac545d4c78d466ec57db3f5145175dbae
Jeong Chong In <chongin12@naver.com>
2024-04-02 14:02:45 +0900

    Initial Commit

---
+++ MosuBlog/MosuBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MosuBlogApp.swift
+//  MosuBlog
+//
+//  Created by 정종인 on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MosuBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
